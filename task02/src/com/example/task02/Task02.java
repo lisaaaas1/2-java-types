a2166151e24a54146728168d42057f4ceb70d54e
el.petryaeva <el.petryaeva@mail.ru>
2024-09-17 17:34:59 +0500

    Петряева Елизавета Задание 02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -5,17 +5,34 @@ public class Task02 {
     public static String solution(String input) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        try {
+            long number = Long.parseLong(input);
+            if( number <= Byte.MAX_VALUE && number >= Byte.MIN_VALUE) {
+                return "Byte";
+            }
+            else if (number <= Short.MAX_VALUE && number >= Short.MIN_VALUE) {
+                return "Short";
+            }
+            else if (number <= Integer.MAX_VALUE && number >= Integer.MIN_VALUE) {
+                return "Integer";
+            }
+            else if (number <= Long.MAX_VALUE && number >= Long.MIN_VALUE) {
+                return "Long";
+            }
+        }
+        catch ( Exception ex) {
+            System.out.println("Ошибка при вводе. Введите число.");
+        }
         return "";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution("12345");
+
+        String result = solution("ssds");
         System.out.println(result);
-         */
+
     }
 
 }
